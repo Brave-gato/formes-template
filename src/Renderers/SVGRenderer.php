815861264e11d79cf4ff361ce4a39c0a937365a0
Gato Dev <webrepoleved@gmail.com>
2023-10-13 16:04:36 +0200

    edit SVGrenderer

--- src/Renderers/SVGRenderer.php
+++ src/Renderers/SVGRenderer.php
@@ -1,17 +1,69 @@
 <?php
-
+ 
 namespace Opmvpc\Formes\Renderers;
-
-class SVGRenderer implements Renderer {
-
-    public function render(): string{
-
+ 
+use Opmvpc\Formes\Canvas;
+use Opmvpc\Formes\Rectangle;
+use Opmvpc\Formes\Cercle;
+use Opmvpc\Formes\Forme;
+use Opmvpc\Formes\Ligne;
+use Opmvpc\Formes\Polygone;
+use PhpParser\Builder\Class_;
+use SVG\Nodes\Shapes\SVGCircle;
+use SVG\Nodes\Shapes\SVGLine;
+use SVG\Nodes\Shapes\SVGPolygon;
+use SVG\Nodes\Shapes\SVGRect;
+use SVG\SVG;
+ 
+class SVGRenderer implements Renderer
+{
+    private Canvas $canvas;
+ 
+    public function __construct(Canvas $canvas)
+    {
+        $this->canvas = $canvas;
     }
+    public function render(): string
+    {
+        $image = new SVG($this->canvas->getWidth(), $this->canvas->getHeight());
+        $doc = $image->getDocument();
+ 
+        $square = new SVGRect(0, 0, $this->canvas->getWidth(), $this->canvas->getHeight(), $this->canvas->getCouleur());
+        $doc->setStyle('fill', $this->canvas->getCouleur());
+        $doc->addChild($square);
 
-    public function save(string $path): void{
 
+        foreach ( $this->canvas->getFormes() as $forme){
+            if(get_class($forme) === Rectangle::class){
+                $rect = new SVGRect($forme->getPoint()->getX(),$forme->getPoint()->getY(),$forme->getWidth(),$forme->getHeight());
+                $rect->setStyle('fill', $forme->getCouleur());
+                $doc->addChild($rect);
+ 
+            }elseif(get_class($forme) === Cercle::class){
+                $cercl = new SVGCircle($forme->getCentre()->getX(),$forme->getCentre()->getY(),$forme->getRayon());
+                $cercl->setStyle('fill', $forme->getCouleur());
+                $doc->addChild($cercl);
+
+          
+            }elseif(get_class($forme) === Ligne::class){
+                $lign = new SVGLine($forme->getPoint1()->getX(),$forme->getPoint2()->getY());
+                $lign->setStyle('fill', $forme->getCouleur());
+                $doc->addChild($lign);
+            }
+
+            }elseif(get_class($forme) === Polygone::class){
+
+            $polyg = new SVGPolygon($forme->getPoints());
+            $doc->addChild($polyg);
+        }
+        
+
+        
+
+ 
+        return $image->toXMLString();
     }
-
-    //public function 
-
+    public function save(string $path): void
+    {
+    }
 }
